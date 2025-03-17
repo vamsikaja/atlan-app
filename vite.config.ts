a9880a42339a98f7bcadc7fc8ac25dcdf02bb5e3
Vamsi Kaja <kajavamsi@gmail.com>
2025-03-17 12:29:08 +0530

    Removed Github action to deploy changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/atlan-app/',
 	plugins: [
 		TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
 		react(),
